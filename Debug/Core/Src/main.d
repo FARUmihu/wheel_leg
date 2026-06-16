03404755583ba0654a5b086658e1be0dc7bdebd2
FARUmihu <335506628@qq.com>
2026-06-17 02:58:35 +0800

    test：3510的测试控制成功；继上次调can之外最牢的一次！

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -30,7 +30,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
  ../Core/Inc/can.h ../Core/Inc/main.h ../Core/Inc/dma.h ../Core/Inc/tim.h \
  ../Core/Inc/usart.h ../Core/Inc/gpio.h \
- C:/Users/FMI/Documents/GitHub/wheel_leg/user/app/app.h
+ C:/Users/FMI/Documents/GitHub/wheel_leg/user/app/app.h \
+ C:/Users/FMI/Documents/GitHub/wheel_leg/user/devices/dm_motor/dm_motor.h \
+ C:/Users/FMI/Documents/GitHub/wheel_leg/user/bsp/bsp_can/bsp_can.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -68,3 +70,5 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/usart.h:
 ../Core/Inc/gpio.h:
 C:/Users/FMI/Documents/GitHub/wheel_leg/user/app/app.h:
+C:/Users/FMI/Documents/GitHub/wheel_leg/user/devices/dm_motor/dm_motor.h:
+C:/Users/FMI/Documents/GitHub/wheel_leg/user/bsp/bsp_can/bsp_can.h:
